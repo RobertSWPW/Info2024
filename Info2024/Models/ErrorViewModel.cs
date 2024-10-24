4e7ed744d8d2e03e0e6fbc8abb666b516b2cefc6
Robert Zak <rzak@wlodkowic.pl>
2024-10-24 20:50:00 +0200

    Dodaj pliki projektów.

---
+++ Info2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Info2024.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
