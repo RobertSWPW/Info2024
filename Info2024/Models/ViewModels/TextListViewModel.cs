160280800ac6fc2245863c171437149f17a45348
Robert Zak <rzak@wlodkowic.pl>
2024-12-03 21:15:41 +0100

    Dodaje filtrowanie danych

--- Info2024/Models/ViewModels/TextListViewModel.cs
+++ Info2024/Models/ViewModels/TextListViewModel.cs
@@ -6,10 +6,13 @@
 		{
 			PageSize = pageSize;
 		}
-
 		public int TextCount { get; set; }
 		public int PageSize { get; set; }
 		public int PageNumber { get; set; }
 		public int PageCount => (int)Math.Ceiling((decimal)TextCount / PageSize);
+		public int? Category { get; set; }
+		public string? Author { get; set; }
+		public string? Phrase { get; set; }
+
 	}
 }
